db018630f409c6ed377a632181e96be87c4948ba
Sebastian Sastre <sebastian@flowingconcept.com>
2015-07-17 16:56:16 -0300

    formatting

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function basicAdd (  ) {
+function basicAdd ( ) {
   var numbers = arguments;
   var sum = _(numbers).reduce( 
     function (total, each) { return total + Number(each) }
@@ -6,7 +6,7 @@ function basicAdd (  ) {
   return sum;
 }
 
-function add(){
+function add ( ) {
   var numbers = arguments;
   return basicAdd.apply( this, numbers );
 }
